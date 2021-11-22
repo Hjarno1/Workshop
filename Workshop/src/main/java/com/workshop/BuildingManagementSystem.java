5e94614b05d13a525e654001815b0435277a6c21
Andreas Trolle <andreas.trolle@hotmail.com>
2021-11-22 08:23:40 +0100

    Implements

--- Workshop/src/main/java/com/workshop/BuildingManagementSystem.java
+++ Workshop/src/main/java/com/workshop/BuildingManagementSystem.java
@@ -1,9 +1,10 @@
 package com.workshop;
 
 import java.util.ArrayList;
+import java.util.Map;
 import java.util.UUID;
 
-public class BuildingManagementSystem {
+public class BuildingManagementSystem implements IBuildingManagementSystem {
     ArrayList<Building> buildings = new ArrayList<Building>();
 
     public ArrayList<Building> getBuildings() {
@@ -25,4 +26,43 @@ public class BuildingManagementSystem {
         }
     }
 
+    @Override
+    public Map<UUID, String> getBuildingInformation() {
+        return null;
+    }
+
+    @Override
+    public Map<UUID, String> getSensorInformation(UUID buildingId) {
+        return null;
+    }
+
+    @Override
+    public Map<UUID, String> getActuatorInformation(UUID buildingId) {
+        return null;
+    }
+
+    @Override
+    public UUID addTemperatureSensor(UUID buildingId, String name) {
+        return null;
+    }
+
+    @Override
+    public UUID addCo2Sensor(UUID buildingId, String name) {
+        return null;
+    }
+
+    @Override
+    public void removeSensor(UUID buildingId, UUID sensorId) {
+
+    }
+
+    @Override
+    public UUID addVentilationActuator(UUID buildingId, String name) {
+        return null;
+    }
+
+    @Override
+    public void removeActuator(UUID buildingId, UUID actuatorId) {
+
+    }
 }
